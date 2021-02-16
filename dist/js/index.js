405b69c93da15c0c79ac4b407223e72ef627e392
Wanderson Silva <wandersonsilva768@gmail.com>
2021-02-16 03:34:24 -0300

    feat: impedindo scroll da página quando menu mobile é aberto

--- dist/js/index.js
+++ dist/js/index.js
@@ -4,6 +4,12 @@
     return document.querySelector.bind(document)(element);
   }
 
+  function noScroll () {
+    document.body.style.overflow = header.classList.contains('open')
+      ? "hidden"
+      : "unset";
+  }
+
   const [ header, headerMenu ] = ['header.header', '#header__menu'].map($);
 
   document.addEventListener('DOMContentLoaded', _ => {
@@ -11,7 +17,12 @@
       e.preventDefault();
 
       header.classList.toggle('open');
+
+      window.scrollTo(0, 0);
+
+      noScroll();
     }, false);
+
   }, false);
 
 })();
